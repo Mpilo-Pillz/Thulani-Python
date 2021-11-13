b3c30940566d336d05fe0172e484c3ed90f772ab
Fikasentani <mpilo.dlamini@yahoo.com>
2021-11-13 06:37:30 +0200

    Angela solution to treasure island

---
+++ hundredDaysOfCode/dayThree/angiesTreasureIsland.py
@@ -0,0 +1,48 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+choice1 = input('You\'re at a crossroads. Choose a direction "left" or "right"?').lower()
+
+if choice1 == "left":
+   choice2 = input("You've come to a lake. There is an island in the middle of the lake. do you want to 'wait' or 'swim'").lower()
+
+   if choice2 == "wait":
+       choice3 = input("You arrive and have to choose one of 3 doors. 'right', 'center', or 'left'?").lower()
+       if choice3 == "center":
+           print("You fall into a pit of fire and burn. Game Over")
+       elif choice3 == "right":
+           print("You found the treasure. You win")
+       elif choice3 == "left":
+           print("The walls close in and crush you")
+       else:
+           print("Went the wrong direction and got bitten by a venomous reptile. Game Over")
+   else:
+       print("You got attacked by a crocodile. Game Over.")
+else:
+    print("You fell into a hole. Game OVer")
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
