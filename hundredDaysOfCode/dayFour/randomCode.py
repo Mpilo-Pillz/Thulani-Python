10a3598b5687403f5a3a817a81c707980931ca21
Fikasentani <mpilo.dlamini@yahoo.com>
2021-11-13 08:10:08 +0200

    looking at random

---
+++ hundredDaysOfCode/dayFour/randomCode.py
@@ -0,0 +1,14 @@
+import random
+random_int = random.randint(0, 11)
+print(random_int)
+
+random_float = random.random()
+print(random_float)
+
+random_decial = random.random() * 10
+
+print(random_decial)
+
+love_score = random.randint(0, 100)
+
+print(f"Your Love Score is {love_score}")
