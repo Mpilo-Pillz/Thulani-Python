8541aebee28683a5767274dc4cbe1543f3f660a9
Fikasentani <mpilo.dlamini@yahoo.com>
2021-11-14 18:21:16 +0200

    looking at random

---
+++ hundredDaysOfCode/dayFour/listFromTheDirt.py
@@ -0,0 +1,35 @@
+cleanFruits = ["Avocados", "Pineapples", "Papayas", "Eggplant", "Kiwifruit", "Honeydew", "Cantaloupe"]
+cleanVeggies = ["Sweet Corn", "Onions", "Frozen sweet peas", "Asparagus", "Broccoli", "Cabbage", "Cauliflower", "Mushrooms"]
+
+dirtyVeggies = ["Spinach", "Kale/Collard/Mustard greens", "Bell and hot peppers", "Celery"]
+dirtyFruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Cherries", "Peaches", "Pears", "Tomatoes"]
+
+cleanTwelve = [cleanFruits, cleanVeggies]
+dirtyDozen = [dirtyVeggies, dirtyFruits]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
