510c8407c9991aae97e2e34ee889d031a74eb76b
Fikasentani <mpilo.dlamini@yahoo.com>
2021-11-15 02:12:55 +0200

    looking at random

---
+++ hundredDaysOfCode/dayFour/mpyList.py
@@ -0,0 +1,10 @@
+list_of_programming_languages = ["JavaScript", "TypeScript", "Python", "Java", "Swift", "Dart", "C Sharp"]
+
+list_of_programming_languages[6] = "C#"
+
+print(list_of_programming_languages)
+
+list_of_programming_languages.append("CSS")
+list_of_programming_languages.extend(["HTML, SQL, JSON"])
+
+print(list_of_programming_languages)
