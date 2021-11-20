c7c25b417592668a06730d449c134b7e458e60d5
Fikasentani <mpilo.dlamini@yahoo.com>
2021-11-20 05:35:58 +0200

    adding my solution to average height

--- hundredDaysOfCode/dayFive/averageHeight.py
+++ hundredDaysOfCode/dayFive/averageHeight.py
@@ -1,8 +1,16 @@
 # 🚨 Don't change the code below 👇
 student_heights = input("Input a list of student heights ").split()
+total_height = 0
 for n in range(0, len(student_heights)):
   student_heights[n] = int(student_heights[n])
 # 🚨 Don't change the code above 👆
+#   print(student_heights)
 
+  total_height += int(student_heights[n])
+  print(total_height)
+
+average_height = total_height / len(student_heights)
+
+print(average_height )
 
 #Write your code below this row 👇
