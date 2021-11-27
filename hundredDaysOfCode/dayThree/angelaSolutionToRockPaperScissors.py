4331cfe154b3b149b37b65f6f1643a1980fab592
Fikasentani <mpilo.dlamini@yahoo.com>
2021-11-27 22:26:01 +0200

    adding Angela's solution to rock paper scissors

---
+++ hundredDaysOfCode/dayThree/angelaSolutionToRockPaperScissors.py
@@ -0,0 +1,52 @@
+from random import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
+game_images = [rock, paper, scissors]
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors\n"))
+print(f"You chose {user_choice}:")
+print(game_images[user_choice])
+if user_choice >= 3 or user_choice < 0:
+    print("You Typed and invalid")
+else:
+    computer_choice = random.randint(0, 2)
+    print(f"Computer chose {computer_choice}:")
+    print(game_images[computer_choice])
+
+
+    if user_choice == 0 and computer_choice == 2:
+        print("YOU WIN")
+    elif computer_choice == 0 and user_choice ==2:
+        print("YOU LOSE")
+    elif computer_choice > user_choice:
+        print("YOU LOSE")
+    elif user_choice > computer_choice:
+        print("YOU WIN")
+    elif computer_choice == user_choice:
+        print("Its a draw")
