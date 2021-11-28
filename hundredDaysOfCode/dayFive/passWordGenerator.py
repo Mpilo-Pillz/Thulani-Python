f951f0a504a7035e214499749c4205dd7db037eb
Fikasentani <mpilo.dlamini@yahoo.com>
2021-11-28 10:06:44 +0200

    adding easy solution to password generator

--- hundredDaysOfCode/dayFive/passWordGenerator.py
+++ hundredDaysOfCode/dayFive/passWordGenerator.py
@@ -8,10 +8,21 @@ print("Welcome to the PyPassword Generator!")
 nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
+password = ""
 
 #Eazy Level - Order not randomised:
 #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+for num in range(0, nr_letters):
+    random_num_in_letters = random.randint(0, len(letters) - 1)
+    password += letters[random_num_in_letters]
 
+for num in range(0, nr_numbers):
+    password += numbers[random.randint(0, 9)]
 
+for num in range(0, nr_symbols):
+    random_symbol_in_symbols = random.randint(0, len(symbols) - 1)
+    password += symbols[random_symbol_in_symbols]
+
+print(password)
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
