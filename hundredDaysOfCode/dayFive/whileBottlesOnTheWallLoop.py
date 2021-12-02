c1a54dd0972de2c803f0e538718135a3291362df
Fikasentani <mpilo.dlamini@yahoo.com>
2021-12-02 03:37:34 +0200

    bottles of beer with while loops

---
+++ hundredDaysOfCode/dayFive/whileBottlesOnTheWallLoop.py
@@ -0,0 +1,6 @@
+number_of_bottles = 100
+
+while number_of_bottles > 0:
+    print(f"{number_of_bottles} number of bottles on the wall. {number_of_bottles} number of bottles on the wall\n Take one down, pass it round");
+    print(f"{number_of_bottles - 1} number of bottles on the wall")
+    number_of_bottles -= 1
