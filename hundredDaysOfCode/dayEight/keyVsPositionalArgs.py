ab47cd3f41e528ae6aae4a312eeec07da53bf6ca
Fikasentani <mpilo.dlamini@yahoo.com>
2021-12-04 20:44:16 +0200

    learning positional and keyword argument

---
+++ hundredDaysOfCode/dayEight/keyVsPositionalArgs.py
@@ -0,0 +1,12 @@
+def myStackPositional(language, frontEndFramework, backEndFramework):
+    print(f"I programme using {language}")
+    print(f"On the Front End I use {frontEndFramework}")
+    print(f"On the backend I use {backEndFramework}")
+
+def myStackKeyword(language, frontEndFramework, backEndFramework):
+    print(f"I programme using {language}")
+    print(f"On the Front End I use {frontEndFramework}")
+    print(f"On the backend I use {backEndFramework}")
+
+myStackKeyword("JavaScript", "Vue", "Express")
+myStackPositional(backEndFramework="Nest", language="TypeScript", frontEndFramework="React")
