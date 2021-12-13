af9f20d2077de0d946eb8dfef03647baea9bdbb4
Fikasentani <mpilo.dlamini@yahoo.com>
2021-12-13 18:16:26 +0200

    adding plans for solution to hang man

--- hundredDaysOfCode/daySeven/main.py
+++ hundredDaysOfCode/daySeven/main.py
@@ -1,3 +1,14 @@
 import random
 from hangman_art import stages, logo
 from hangman_words import word_list
+
+# declare number of wrongs
+# while number of wrongs is less than stages.lenth
+#     input guess the letter
+#     check letter is in word
+#  if word is not in letter
+#   number of plus1
+#   print stages length - number of wrond
+#  else
+#     replace _ with word
+#   print _ with words
