67f7165872281c27bbfccd4545bb336b89b2a29c
mpilopillz <mpilo.dlamini@yahoo.com>
2021-12-20 23:19:13 +0200

    finishing up my version of password gen hard mode

--- hundredDaysOfCode/dayFive/passWordGenerator.py
+++ hundredDaysOfCode/dayFive/passWordGenerator.py
@@ -24,13 +24,25 @@ for num in range(0, nr_symbols):
     password += symbols[random_symbol_in_symbols]
 
 print(password)
-convert_password_to_array = list(password)
-print(convert_password_to_array)
-shuffle_password_array = random.shuffle(convert_password_to_array)
-print(shuffle_password_array)
+# convert_password_to_array = list(password)
+# print('-->',convert_password_to_array)
+# shuffle_password_array = random.shuffle(convert_password_to_array)
+# random.shuffle(random.shuffle(convert_password_to_array))
+print(list(password))
+print('-->',random.shuffle(list(password)))
+shuffled = random.sample(password, len(password))
+print("shuf-->", shuffled)
+
+# print(shuffle_password_array)
 # shuffle_password = ''.join(shuffle_password_array)
 # print(f"Shuffled word is {shuffle_password}")
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
 
 # TODO: shuffle the words
+shuffledPAssword = ""
+
+for word in shuffled:
+    shuffledPAssword += word
+
+print(f"your shuffled password is {shuffledPAssword}")
