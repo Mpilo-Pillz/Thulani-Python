e6390fa5672fc1e76e134c15e665db43ef0e8761
mpilopillz <mpilo.dlamini@yahoo.com>
2021-12-21 22:04:57 +0200

    selecting random word and prining line version

--- hundredDaysOfCode/daySeven/main.py
+++ hundredDaysOfCode/daySeven/main.py
@@ -2,6 +2,16 @@ import random
 from hangman_art import stages, logo
 from hangman_words import word_list
 
+# Select a random word
+word_answer = word_list[random.randint(0, len(word_list) - 1)]
+obscurial = ""
+print(word_answer)
+
+for word in word_answer:
+    obscurial += '_'
+
+print(obscurial)
+
 # declare number of wrongs
 # while number of wrongs is less than stages.lenth
 #     input guess the letter
