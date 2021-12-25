11d3c0144d663dd04d7c628764740fb30a4877ed
mpilopillz <mpilo.dlamini@yahoo.com>
2021-12-25 21:23:50 +0200

    HANGMAN - solution to challenge 2 replacing blanks with valid letter

--- hundredDaysOfCode/daySeven/hangmanSolution.py
+++ hundredDaysOfCode/daySeven/hangmanSolution.py
@@ -3,10 +3,26 @@ from hangman_art import stages, logo
 from hangman_words import word_list
 
 chosen_word = random.choice(word_list)
+print(f"Psst the word is {chosen_word}")
+
+display = []
+word_length = len(chosen_word)
+
+for _ in chosen_word:
+    display += "_"
+print(display)
+
 guess = input("Guess a letter \n").lower()
 
-for letter in chosen_word:
+for position in range(word_length):
+    letter = chosen_word[position]
     if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+        display[position] = letter
+
+print(display)
+# for letter in chosen_word:
+#     if letter == guess:
+#         print("Right")
+#     else:
+#         print("Wrong")
+
