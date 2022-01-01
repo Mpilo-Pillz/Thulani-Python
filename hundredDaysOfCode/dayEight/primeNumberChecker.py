4ed3a6f96419354ce8e0e39bc44c5a3378974863
Fikasentani <mpilo.dlamini@yahoo.com>
2022-01-01 19:59:01 +0200

    Prime checker - my version of prime checker

--- hundredDaysOfCode/dayEight/primeNumberChecker.py
+++ hundredDaysOfCode/dayEight/primeNumberChecker.py
@@ -1,14 +1,24 @@
 # Write your code below this line 👇
 def prime_checker(number):
+    is_prime = False
     if number == 0:
         print("Zero is not prime")
 
     elif number > 0 and number < 4:
+        is_prime = True
+    else:
+        for num in range(2, number):
+            # print(num)
+            if number % num == 0:
+                is_prime = False
+                break
+            else:
+                is_prime = True
+
+    if is_prime:
         print(f"{number} is prime")
     else:
-        for num in range(number):
-            print(num)
-
+        print(f"{number} is NOT prime")
 # Write your code above this line 👆
 
 # Do NOT change any of the code below👇
