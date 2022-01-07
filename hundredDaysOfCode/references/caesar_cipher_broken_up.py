6341e9fe9e20d4fbe05d335d7e70b85d95f3a65d
mpilopillz <mpilo.dlamini@yahoo.com>
2022-01-07 21:12:40 +0200

    caesar_cipher - running decript based on number of items in array

---
+++ hundredDaysOfCode/references/caesar_cipher_broken_up.py
@@ -0,0 +1,17 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+word_array = ["mpilo", "fikasentech"]
+def decrypt(text, shift):
+    decrypted_word = ""
+
+    for letter in range(len(text)):
+        indexOfWordInAlphabet = alphabet.index(text[letter])
+
+        if text[letter] in alphabet:
+            if indexOfWordInAlphabet - shift >= 0:
+                decrypted_word += alphabet[(indexOfWordInAlphabet - shift) - (len(alphabet))]
+            else:
+                decrypted_word += alphabet[(alphabet.index(text[letter]) - shift) + (len(alphabet))]
+    print(f"{text} decrypted by shift number {shift} is {decrypted_word}")
+
+for word in word_array:
+    decrypt(word, 2)
