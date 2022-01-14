078a26411bd136b0eef08c171e9c253f2670f749
mpilopillz <mpilo.dlamini@yahoo.com>
2022-01-14 20:17:46 +0200

    day 9 - creating empty dictionary and adding exceeds expectations grade

---
+++ hundredDaysOfCode/dayNine/student_grades.py
@@ -0,0 +1,20 @@
+student_scores = {
+    "Harry": 81,
+    "Ron": 78,
+    "Hermione": 99,
+    "Draco": 74,
+    "Neville": 62,
+}
+# 🚨 Don't change the code above 👆
+
+# TODO-1: Create an empty dictionary called student_grades.
+student_grades = {}
+
+# TODO-2: Write your code below to add the grades to student_grades.👇
+for student_score in student_scores:
+    if student_scores[student_score] >= 91 and student_scores[student_score] <= 100:
+        student_grades[student_score] = "Outstanding"
+    elif student_scores[student_score] >= 81 and student_scores[student_score] <= 90:
+        student_grades[student_score] = "Exceeds Expectations"
+# 🚨 Don't change the code below 👇
+print(student_grades)
