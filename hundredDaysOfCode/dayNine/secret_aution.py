963bd0372e9ce3797b3b2d173922272e36e4ed0f
mpilopillz <mpilo.dlamini@yahoo.com>
2022-01-19 21:24:09 +0200

    day 9 - adding my solution to secret auction

--- hundredDaysOfCode/dayNine/secret_aution.py
+++ hundredDaysOfCode/dayNine/secret_aution.py
@@ -2,16 +2,26 @@ from secret_auction_logo import logo
 
 print(logo)
 
-bids = []
-bidder = {}
+# bids = []
+bidders = {}
+highestBid = 0
+highestBidder = ""
 thereAreMoreBids = True
 while thereAreMoreBids:
     biddersName = input("What is your name? ")
     bidingAmount = input("What is your bid? ")
-    bidder[biddersName] = bidingAmount
+    bidders[biddersName] = bidingAmount
 
     isThereMoreBids = input("Are there any more bidders?")
 
     if isThereMoreBids.lower() == "no":
         thereAreMoreBids = False
-print(bidder)
+print(bidders)
+
+for bidder in bidders:
+    # print(bidders[bidder])
+    if int(bidders[bidder]) > highestBid:
+        highestBid = int(bidders[bidder])
+        highestBidder = bidder
+
+print(f"{highestBidder} has the highest bid, bidding R{highestBid} per hour as the amount")
