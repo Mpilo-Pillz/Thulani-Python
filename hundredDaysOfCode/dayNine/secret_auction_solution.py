f60bc132e7349ba1d322879fe9c4899b733c479d
mpilopillz <mpilo.dlamini@yahoo.com>
2022-01-20 17:42:01 +0200

    day 9 - solution wip looping until user selects no

--- hundredDaysOfCode/dayNine/secret_auction_solution.py
+++ hundredDaysOfCode/dayNine/secret_auction_solution.py
@@ -3,7 +3,12 @@ from secret_auction_logo import logo
 print(logo)
 
 bids = {}
+biffing_finished = False
 
-name = input("What is your name?")
-price = input("What is your bid? $")
-bids[name] = price
+while not biffing_finished:
+    name = input("What is your name?")
+    price = input("What is your bid? $")
+    bids[name] = price
+    should_continue = input("Are there any other bidders? Type 'yes' or 'no'.")
+    if should_continue == "no":
+        biffing_finished = True
