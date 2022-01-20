f86abc83aa5b2ff672842fe95c8091abd317d587
mpilopillz <mpilo.dlamini@yahoo.com>
2022-01-20 17:48:50 +0200

    day 9 - solution wip fixing spelling mistake and supposed to be clearing terminal if yes is selected

--- hundredDaysOfCode/dayNine/secret_auction_solution.py
+++ hundredDaysOfCode/dayNine/secret_auction_solution.py
@@ -3,12 +3,14 @@ from secret_auction_logo import logo
 print(logo)
 
 bids = {}
-biffing_finished = False
+bidding_finished = False
 
-while not biffing_finished:
+while not bidding_finished:
     name = input("What is your name?")
     price = input("What is your bid? $")
     bids[name] = price
     should_continue = input("Are there any other bidders? Type 'yes' or 'no'.")
     if should_continue == "no":
-        biffing_finished = True
+        bidding_finished = True
+    elif should_continue == "yes":
+        print("--------------------Supposed to clear but clearing terminal is hard with Python----------------")
