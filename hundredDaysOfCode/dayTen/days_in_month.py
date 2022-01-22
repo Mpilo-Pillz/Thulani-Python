f7a74aa2755856614f63462f059591a6dec5d1e2
Fikasentani <mpilo.dlamini@yahoo.com>
2022-01-22 14:52:34 +0200

    day 10 - my solution to days in month

--- hundredDaysOfCode/dayTen/days_in_month.py
+++ hundredDaysOfCode/dayTen/days_in_month.py
@@ -3,16 +3,30 @@ def is_leap(year):
         if year % 100 == 0:
             if year % 400 == 0:
                 print("Leap year.")
+                return True
             else:
                 print("Not leap year.")
+                return False
         else:
             print("Leap year.")
+            return True
     else:
         print("Not leap year.")
+        return False
 
 
-def days_in_month():
+def days_in_month(year, month_number):
     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    if is_leap(year) and month_number == 2:
+        print(month_days[1])
+        print(month_days[1] + 1)
+        print(month_days)
+        month_days[1] = month_days[1] + 1
+        print(month_days)
+        return month_days[month_number]
+    else:
+        return month_days[month_number]
 
 # 🚨 Do NOT change any of the code below
 year = int(input("Enter a year: "))
