a1b329573aa818df7c76ea01eaea20b25ce3a8b9
Fikasentani <mpilo.dlamini@yahoo.com>
2022-01-23 10:02:12 +0200

    day 10 - WIP cleaning up calculator art

--- hundredDaysOfCode/dayTen/calculator_art.py
+++ hundredDaysOfCode/dayTen/calculator_art.py
@@ -14,22 +14,4 @@ logo = """
 | |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
 |_____________________|
 """
-def calculate(first_num, second_num, operator):
-    total = 0
-    if (operator == "-"):
-        total -= subtract(int(first_num), int(second_num))
-    return total
 
-def add(first_num, second_num):
-    return f"{first_num} + {second_num} = {first_num + second_num}"
-
-def subtract(first_num, second_num):
-    return f"{first_num} - {second_num} = {first_num - second_num}"
-
-def divide(first_num, second_num):
-    if first_num == 0 or second_num == 0:
-        return "error cannot divide by 0"
-    return f"{first_num} / {second_num} = {first_num / second_num}"
-
-def multiply(first_num, second_num):
-    return f"{first_num} * {second_num} = {first_num * second_num}"
