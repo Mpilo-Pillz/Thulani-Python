cac35401e9e7466ffa6feef66eb2d53e0b4b7f49
Fikasentani <mpilo.dlamini@yahoo.com>
2022-01-23 10:12:35 +0200

    day 10 - WIP creating operators object

---
+++ hundredDaysOfCode/dayTen/calculator_solution.py
@@ -0,0 +1,20 @@
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multily(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multily,
+    "/": divide
+}
+
+# num1 = int(input("What is the"))
