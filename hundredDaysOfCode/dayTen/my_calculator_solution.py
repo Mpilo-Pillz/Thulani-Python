4b78ce5fd208e3839e93cd7e48b9ce7edd792646
Fikasentani <mpilo.dlamini@yahoo.com>
2022-01-28 18:34:50 +0200

    day 10 - wip performing arothmetc through return

--- hundredDaysOfCode/dayTen/my_calculator_solution.py
+++ hundredDaysOfCode/dayTen/my_calculator_solution.py
@@ -1,19 +1,43 @@
-def calculate(first_num, second_num, operator):
-    total = 0
-    if (operator == "-"):
-        total -= subtract(int(first_num), int(second_num))
-    return total
+from calculator_art import logo
+
+print(logo)
+first_number = int(input("What is the first number?: "))
+def calculate():
+    second_number = int(input("What is the next number?: "))
+    print(operators)
+    operation = input("Chose from the list of operators above to perform a calculation: ")
+    # answer = performCalculation(operators[operation], first_number, second_number)
+    answer = operators[operation](first_number, second_number) # too fancy
+    return f"{first_number} {operation} {second_number} = {answer}"
+
 
 def add(first_num, second_num):
-    return f"{first_num} + {second_num} = {first_num + second_num}"
+    return first_num + second_num
+    # return f"{first_num} + {second_num} = {first_num + second_num}"
 
 def subtract(first_num, second_num):
-    return f"{first_num} - {second_num} = {first_num - second_num}"
+    return first_num - second_num
+    # return f"{first_num} - {second_num} = {first_num - second_num}"
 
 def divide(first_num, second_num):
     if first_num == 0 or second_num == 0:
         return "error cannot divide by 0"
-    return f"{first_num} / {second_num} = {first_num / second_num}"
+    return first_num / second_num
+    # return f"{first_num} / {second_num} = {first_num / second_num}"
 
 def multiply(first_num, second_num):
-    return f"{first_num} * {second_num} = {first_num * second_num}"
+    return first_num * second_num
+    # return f"{first_num} * {second_num} = {first_num * second_num}"
+
+def performCalculation(operation, num1, num2):
+    return operation(num1, num2)
+
+operators = {
+    '*': multiply,
+    '/': divide,
+    '+': add,
+    '-': subtract
+}
+
+print(calculate())
+
