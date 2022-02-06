09caf392fb61ca7e3238cbc95e785197c1fd0870
Fikasentani <mpilo.dlamini@yahoo.com>
2022-02-06 13:12:03 +0200

    day-11 getting basic functionality of the program with spahghetti code

--- hundredDaysOfCode/dayEleven/blackjack.py
+++ hundredDaysOfCode/dayEleven/blackjack.py
@@ -6,6 +6,8 @@ users_hand = []
 computers_hand = []
 users_score = 0
 computers_score = 0
+black_jack = 21
+
 
 user_first_card = cards[random.randint(0, (len(cards) - 1))]
 user_second_card = cards[random.randint(0, (len(cards) - 1))]
@@ -22,19 +24,57 @@ def players_hand(player_score, players_hand):
         player_score += card
     return player_score
 
+
 computers_score = players_hand(computers_score, computers_hand)
 users_score = players_hand(users_score, users_hand)
 print(logo)
-print(f"Your cards: {users_hand} Your score: {users_score}")
-print(f"-----------DUBUG-----Computers cards: {computers_hand} Computer score: {computers_score}-------------")
-print(f"Computers first hand: {computers_hand[0]}")
 
-if int(users_score) > int(computers_score):
-    print("You win 😃")
-elif int(users_score) < int(computers_score):
-    print("You Lose 😭")
-else:
-    print("It is a Draw 😱")
+
+def black_jack_game(users_score, computers_score):
+    game_is_on = True
+
+    if users_score < 21 and computers_score < 21:
+        while game_is_on:
+            print(f"Your cards: {users_hand} Your score: {users_score}")
+            print(f"-----------DUBUG-----Computers cards: {computers_hand} Computer score: {computers_score}-------------")
+            print(f"Computers first hand: {computers_hand[0]}")
+            hit_user = input("Type 'y' to get another card, type 'n' to pass: ")
+            if hit_user.lower() == 'y':
+                users_hand.append(cards[random.randint(0, (len(cards) - 1))])
+                computers_hand.append(cards[random.randint(0, (len(cards) - 1))])
+                computers_score += computers_hand[-1]
+                users_score += users_hand[-1]
+                if users_score > 21:
+                    print("You went over, YOU Lose")
+                    game_is_on = False
+                    return
+                if computers_score > 21:
+                    print("You WIN,computer over")
+                    game_is_on = False
+                    return
+                if users_score == black_jack:
+                    print("You WIN by BLACKJACK")
+                    game_is_on = False
+                    return
+                if computers_score > 21:
+                    print("You Lose,computer got BLACKJACK")
+                    game_is_on = False
+                    return
+                black_jack_game(users_score, computers_score)
+            else:
+                game_is_on = False
+                if users_score == 21:
+                    print("You win you scored Black Jack 😃")
+                elif computers_score == 21:
+                    print("You lose computer scored Black Jack 😫")
+                if int(users_score) > int(computers_score) and users_score <= black_jack:
+                    print("You win 😃")
+                elif int(users_score) < int(computers_score) and computers_score <= black_jack:
+                    print("You Lose 😭")
+                else:
+                    print("It is a Draw 😱")
+
+
 # Get the winner by random
 # add option to hit
 #  - append to user score
@@ -44,5 +84,5 @@ else:
 
 # while yes
 # terminate on no
-
+black_jack_game(users_score, computers_score)
 
