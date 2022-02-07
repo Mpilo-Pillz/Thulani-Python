ad7066226166de8cdd784397d11d624cdfd91366
Fikasentani <mpilo.dlamini@yahoo.com>
2022-02-07 22:02:48 +0200

    day-11 showing user cards and computer first card

--- hundredDaysOfCode/dayEleven/black_jack_solution.py
+++ hundredDaysOfCode/dayEleven/black_jack_solution.py
@@ -5,15 +5,8 @@ def deal_card():
     card = random.choice(cards)
     return card
 
-#Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
-user_cards = []
-computer_cards = []
-
-for _ in range(2):
-    user_cards.append(deal_card())
-    computer_cards.append(deal_card())
-
 def calculate_score(cards):
+    """Take a list of cards and return the score calculated from the cards"""
     # Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10)
     # and return 0 instead of the actual score. 0 will represent a blackjack in our game.
     if sum(cards) == 21 and len(cards) == 2:
@@ -24,6 +17,24 @@ def calculate_score(cards):
         cards.append(1)
 #Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
     return sum(cards)
+
+#Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
+user_cards = []
+computer_cards = []
+is_game_over = False
+
+for _ in range(2):
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
+
+user_score = calculate_score(user_cards)
+computer_score = calculate_score(computer_cards)
+print(f"    Your cards: {user_cards}, current score: {user_score}")
+print(f"    computers first card: {computer_cards[0]}")
+
+if user_score == 0 or computer_score == 0 or user_score > 21:
+    is_game_over = True
+
 #Hint 10: If the game has not ended, ask the user if they want to draw another card. If yes, then use the deal_card() function to add another card to the user_cards List. If no, then the game has ended.
 
 #Hint 11: The score will need to be rechecked with every new card drawn and the checks in Hint 9 need to be repeated until the game ends.
