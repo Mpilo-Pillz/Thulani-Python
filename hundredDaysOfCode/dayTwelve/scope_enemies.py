afb33b1bc5969d30578bd35463351afccfbc38ff
Fikasentani <mpilo.dlamini@yahoo.com>
2022-02-10 14:58:23 +0200

    day-12 looking at globa scope

--- hundredDaysOfCode/dayTwelve/scope_enemies.py
+++ hundredDaysOfCode/dayTwelve/scope_enemies.py
@@ -8,3 +8,12 @@ def increase_enemies():
 
 increase_enemies()
 print(f"enemies outside function: {enemies}")
+
+player_health = 10
+
+def drink_potion():
+  potion_strength = 2
+  print(potion_strength + player_health)
+
+drink_potion()
+
