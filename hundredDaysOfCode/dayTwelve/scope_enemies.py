02cc079f278ae062a69332fb20a54112b6a45bb5
Fikasentani <mpilo.dlamini@yahoo.com>
2022-02-12 09:26:53 +0200

    day-12 scoping with functions

--- hundredDaysOfCode/dayTwelve/scope_enemies.py
+++ hundredDaysOfCode/dayTwelve/scope_enemies.py
@@ -15,14 +15,22 @@ def drink_potion():
   potion_strength = 2
   print(potion_strength + player_health)
 
+
+
 drink_potion()
 
 # BLOCK SCOPE
 game_level = 3
 enemies = ["Skeleton", "Zombie", "Alien"]
-
+def create_ally():
+  allies = ["Amazon", "Paladin", "Barbarian"]
+  if game_level < 10:
+    new_ally = allies[1]
+  print(new_ally)
 if game_level < 5:
   new_enemy = enemies[0]
 
+
 print(new_enemy)
+# print(new_ally) Errors as it is scopped by function
 
