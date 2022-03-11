4786e7d85cf02ed52249a693eda0b57153d762c2
Fikasentani <mpilo.dlamini@yahoo.com>
2022-03-11 19:40:10 +0200

    day-12 printing number guess

--- hundredDaysOfCode/dayTwelve/guess_the_number.py
+++ hundredDaysOfCode/dayTwelve/guess_the_number.py
@@ -1,3 +1,12 @@
 from art_guess_the_number import logo
 
 print(logo)
+print("I'm thinking of a number between 1 and 100")
+
+def checkGuess(number, user_guess):
+    if number > user_guess:
+        return "Your guess is too high"
+    elif number < user_guess:
+        return "Your guess is too low"
+    else:
+        return "You guess is correct"
