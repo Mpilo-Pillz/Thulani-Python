448e7ff1cb0182a8e6552f3d67b2c457bf73555a
Fikasentani <mpilo.dlamini@yahoo.com>
2022-03-13 10:16:15 +0200

    day-12: Generating random number

--- hundredDaysOfCode/dayTwelve/guess_the_number.py
+++ hundredDaysOfCode/dayTwelve/guess_the_number.py
@@ -1,7 +1,17 @@
+import random
 from art_guess_the_number import logo
 
 print(logo)
-print("I'm thinking of a number between 1 and 100")
+# print("I'm thinking of a number between 1 and 100: ")
+
+number_to_guess = random.randint(0, 100 - 1)
+number_of_tries_left = 10
+difficulty = input("Type 'easy' or 'hard' for difficulty")
+
+if difficulty == 'hard':
+    number_of_tries_left = 5
+
+user_guess = input("I'm thinking of a number between 1 and 100: ")
 
 def checkGuess(number, user_guess):
     if number > user_guess:
