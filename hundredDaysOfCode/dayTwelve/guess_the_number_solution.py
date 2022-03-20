8b7c8e6d24fd54978739b2f46cf10d6c030ced34
Fikasentani <mpilo.dlamini@yahoo.com>
2022-03-20 09:08:58 +0200

    day-12: adding check answer function

--- hundredDaysOfCode/dayTwelve/guess_the_number_solution.py
+++ hundredDaysOfCode/dayTwelve/guess_the_number_solution.py
@@ -1,6 +1,20 @@
+from random import randint
+
+# Function to check user's guess against actual answer
+def check_answer(guess, answer):
+    if guess > answer:
+        print("Too High")
+    elif guess < answer:
+        print("Too Low")
+    else:
+        print(f"You got it. The answer was {answer}.")
+print("Welcome to the guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
 # Choose random number between 1 and 100
+answer = randint(1, 100)
 # Make function to set difficulty
 # Let the user guess a number
-# Function to check user's guess against actual answer
+guess = int(input("Make a guess:"))
+
 # Track the number of tunrs and reduce by 1 if they get it wrong
 # Repeat the guessing functionality if they get it wrong
