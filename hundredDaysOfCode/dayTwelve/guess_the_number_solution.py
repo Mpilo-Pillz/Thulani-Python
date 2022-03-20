0f0a2c9ae94fcc378a18b6f993668b671d6c97fc
Fikasentani <mpilo.dlamini@yahoo.com>
2022-03-20 09:26:20 +0200

    day-12: checking guess and setting difficulty

--- hundredDaysOfCode/dayTwelve/guess_the_number_solution.py
+++ hundredDaysOfCode/dayTwelve/guess_the_number_solution.py
@@ -1,5 +1,7 @@
 from random import randint
 
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
 # Function to check user's guess against actual answer
 def check_answer(guess, answer):
     if guess > answer:
@@ -8,13 +10,27 @@ def check_answer(guess, answer):
         print("Too Low")
     else:
         print(f"You got it. The answer was {answer}.")
-print("Welcome to the guessing Game!")
+
+# Make function to set difficulty
+def set_difficulty():
+    level = input("Choose a difficulty. Type 'easy' or 'hard': ")
+    if level == "easy":
+        return EASY_LEVEL_TURNS
+    else:
+        return HARD_LEVEL_TURNS
+print("Welcome to the number guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
 # Choose random number between 1 and 100
 answer = randint(1, 100)
-# Make function to set difficulty
+print(f"Psst, the answer is {answer}")
+
+turns = set_difficulty()
+print(f"You have {turns} attempts remaining to guess the number")
 # Let the user guess a number
-guess = int(input("Make a guess:"))
+guess = int(input("Make a guess: "))
+
+check_answer(guess, answer)
+
 
 # Track the number of tunrs and reduce by 1 if they get it wrong
 # Repeat the guessing functionality if they get it wrong
