92b99e0b1803b27c745b35da43c4b4b6d9649b8a
Fikasentani <mpilo.dlamini@yahoo.com>
2022-03-20 08:59:09 +0200

    day-12: adding solution notes

---
+++ hundredDaysOfCode/dayTwelve/guess_the_number_solution.py
@@ -0,0 +1,6 @@
+# Choose random number between 1 and 100
+# Make function to set difficulty
+# Let the user guess a number
+# Function to check user's guess against actual answer
+# Track the number of tunrs and reduce by 1 if they get it wrong
+# Repeat the guessing functionality if they get it wrong
