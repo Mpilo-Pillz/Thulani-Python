8753ef46662ee531dd8104bfb23bb435337a8543
Fikasentani <mpilo.dlamini@yahoo.com>
2022-03-24 22:49:21 +0200

    day-13: adding all the game logic

--- hundredDaysOfCode/beginner/dayFourteen/higher_lower.py
+++ hundredDaysOfCode/beginner/dayFourteen/higher_lower.py
@@ -7,18 +7,35 @@ score = 0
 print(logo)
 option_a = data[randint(0, len(data) - 1)]
 option_b = data[randint(0, len(data) - 1)]
-print(f'compare A: {option_a["name"]}, a {option_a["description"]}, from {option_a["country"]}')
-print(vs)
-print(f'compare B: {option_b["name"]}, a {option_b["description"]}, from {option_b["country"]}')
+game_is_on = True
+while game_is_on:
 
-user_option = input("Who has more followers? Type 'A' or 'B': ")
 
-if option_a['follower_count'] < option_b['follower_count']:
-    correct_answer = 'B'
-else:
-    correct_answer = 'A'
+    # print(option_a)
+    # print(f"b->{option_b}")
+    if option_a['follower_count'] < option_b['follower_count']:
+        correct_answer = 'B'
+    else:
+        correct_answer = 'A'
 
-if user_option == correct_answer:
-    score += 1
-print(correct_answer)
-print(score)
+    print(f"Correct answer {correct_answer}")
+    print(f'compare A: {option_a["name"]}, a {option_a["description"]}, from {option_a["country"]}')
+    print(vs)
+    print(f'compare B: {option_b["name"]}, a {option_b["description"]}, from {option_b["country"]}')
+
+    user_option = input("Who has more followers? Type 'A' or 'B': ")
+
+    if user_option.upper() == correct_answer:
+        score += 1
+        print(f"You're right! Current score: {score}")
+        option_a = option_b
+        option_b = data[randint(0, len(data) - 1)]
+    else:
+        print(f"Sorry, that's wrong. Final score: {score}")
+        game_is_on = False
+
+
+
+
+    # print(f"Op A{option_a}")
+    # print(f"Op B{option_b}")
