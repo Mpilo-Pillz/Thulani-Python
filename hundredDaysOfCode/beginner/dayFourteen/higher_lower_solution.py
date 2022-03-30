067a8e29dd0fb3bb364200f963e1c063a8bfa6c8
Fikasentani <mpilo.dlamini@yahoo.com>
2022-03-30 18:33:20 +0200

    day-14: creating data formatter function

--- hundredDaysOfCode/beginner/dayFourteen/higher_lower_solution.py
+++ hundredDaysOfCode/beginner/dayFourteen/higher_lower_solution.py
@@ -1,18 +1,27 @@
 from art import logo
 from game_data import data
 import random
+
+def format_data(account):
+    """Format the account data into a printable format."""
+    account_name = account["name"]
+    account_descr = account["description"]
+    account_country = account["country"]
+    return f"{account_name}, a {account_descr}, from {account_country}"
 # Display art
 print(logo)
 
 # Generate random account game data
-
-# Format the account data into a printable format
 account_a = random.choice(data)
 account_b = random.choice(data)
 
 # check that the accounts are not the same
 if account_a == account_b:
     account_b = random.choice(data)
+
+print(f"Compare A: {format_data(account_a)}")
+print(f"Compare B: {format_data(account_b)}")
+
 # Ask user for a guess
 
 # Check if user is correct
