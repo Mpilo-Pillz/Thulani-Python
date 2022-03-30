49a84b9170c812ab5cdc9ea4b90e666da9770da1
Fikasentani <mpilo.dlamini@yahoo.com>
2022-03-30 18:06:56 +0200

    day-14: setting account a and b and making sure thst they are not the same

--- hundredDaysOfCode/beginner/dayFourteen/higher_lower_solution.py
+++ hundredDaysOfCode/beginner/dayFourteen/higher_lower_solution.py
@@ -1,9 +1,18 @@
+from art import logo
+from game_data import data
+import random
 # Display art
+print(logo)
 
 # Generate random account game data
 
 # Format the account data into a printable format
+account_a = random.choice(data)
+account_b = random.choice(data)
 
+# check that the accounts are not the same
+if account_a == account_b:
+    account_b = random.choice(data)
 # Ask user for a guess
 
 # Check if user is correct
