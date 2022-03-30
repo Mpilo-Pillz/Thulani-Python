6a9560fc5927d7e9e36c4129d05c6d6437d45b13
Fikasentani <mpilo.dlamini@yahoo.com>
2022-03-30 18:02:23 +0200

    day-14: adding solution notes

---
+++ hundredDaysOfCode/beginner/dayFourteen/higher_lower_solution.py
@@ -0,0 +1,21 @@
+# Display art
+
+# Generate random account game data
+
+# Format the account data into a printable format
+
+# Ask user for a guess
+
+# Check if user is correct
+# Get the follwer count for each account
+# Use if statement to check if user is correct
+
+# Give user feedback
+
+# Score keeping
+
+# Make the game repeatable
+
+# Making the account at position b be at position a
+
+# Clear the screen
