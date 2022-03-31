60f5e0316d0a74768307ba640d352455a43ed8f0
Fikasentani <mpilo.dlamini@yahoo.com>
2022-03-31 12:12:45 +0200

    commit message

--- hundredDaysOfCode/beginner/dayFourteen/higher_lower_solution.py
+++ hundredDaysOfCode/beginner/dayFourteen/higher_lower_solution.py
@@ -3,41 +3,61 @@ from game_data import data
 import random
 
 def format_data(account):
-    """Format the account data into a printable format."""
+    """Takes the account data and returns a printable format."""
     account_name = account["name"]
     account_descr = account["description"]
     account_country = account["country"]
     return f"{account_name}, a {account_descr}, from {account_country}"
+
+def check_answer(guess, a_followers, b_followers):
+    """Takes the users guess and follower count and returns if they got it right"""
+    if a_followers > b_followers:
+        return guess == 'a'
+    else:
+        return guess == "b"
+
+
 # Display art
 print(logo)
-
-# Generate random account game data
-account_a = random.choice(data)
+score = 0
+game_should_continue = True
 account_b = random.choice(data)
 
-# check that the accounts are not the same
-if account_a == account_b:
+while game_should_continue:
+    # Generate random account game data
+    account_a = account_b
     account_b = random.choice(data)
 
-print(f"Compare A: {format_data(account_a)}")
-print(vs)
-print(f"Compare B: {format_data(account_b)}")
+    # check that the accounts are not the same
+    while account_a == account_b:
+        account_b = random.choice(data)
 
-# Ask user for a guess
-guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+    print(f"Compare A: {format_data(account_a)}")
+    print(vs)
+    print(f"Compare B: {format_data(account_b)}")
 
-# Check if user is correct
-# Get the follwer count for each account
-a_follower_count = account_a["follower_count"]
-b_follower_count = account_b["follower_count"]
-# Use if statement to check if user is correct
+    # Ask user for a guess
+    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
 
-# Give user feedback
+    # Check if user is correct
+    # Get the follwer count for each account
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
 
-# Score keeping
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
 
-# Make the game repeatable
+    # Give user feedback
+    if is_correct:
+        score += 1
+        print(f"You're right! Current score: {score}.")
+    else:
+        game_should_continue = False
+        print(f"Sorry, that's wrong. Final score: {score}")
 
-# Making the account at position b be at position a
+    # Score keeping
+
+    # Make the game repeatable
+
+    # Making the account at position b be at position a
 
 # Clear the screen
