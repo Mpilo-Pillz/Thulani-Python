7450127dde2587655f2121612dd018db43dd59d4
Fikasentani <mpilo.dlamini@yahoo.com>
2022-04-05 21:08:08 +0200

    day-16: Printing pokemon table

--- hundredDaysOfCode/intermediate/day_16/beautifulTable.py
+++ hundredDaysOfCode/intermediate/day_16/beautifulTable.py
@@ -2,4 +2,6 @@ from prettytable import PrettyTable
 table = PrettyTable()
 table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander", "Bulbasaur"])
 table.add_column("Type", ["Electric", "Water", "Fire", "Grass"])
+
+table.align = "l"
 print(table)
