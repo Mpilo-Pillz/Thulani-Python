6413c9b5aab5984ee8e9ca9b1429a2d5f1ac19fa
mpilopillz <mpilo.dlamini@yahoo.com>
2022-05-02 22:13:59 +0200

    day 12 - looking at scope error

---
+++ hundredDaysOfCode/references/scope_error.py
@@ -0,0 +1,19 @@
+def a_function(a_parameter):
+    a_variable = 15
+    return a_parameter
+
+
+a_function(10)
+print(a_variable)
+
+# FOO
+i = 50
+
+
+def foo():
+    i = 100
+    return i
+
+
+foo()
+print(i)
