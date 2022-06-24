9e238e3fbf13bd80fa2247885bc075d42850f253
Fikasentani <mpilo.dlamini@yahoo.com>
2022-06-24 14:55:45 +0200

    printing what user would like

--- hundredDaysOfCode/intermediate/day_16/oop_coffee_machine/main.py
+++ hundredDaysOfCode/intermediate/day_16/oop_coffee_machine/main.py
@@ -1,3 +1,9 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+coffee_maker = CoffeeMaker()
+menu = Menu()
+
+print(f"What would you linke {menu.get_items()}")
+coffee_maker.report()
