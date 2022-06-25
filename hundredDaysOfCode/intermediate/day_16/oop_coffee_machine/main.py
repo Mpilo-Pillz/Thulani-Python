d7fe4565e7e1ba1be074635101daacdb5b6363b7
Fikasentani <mpilo.dlamini@yahoo.com>
2022-06-25 15:46:28 +0200

    day-16: printing report

--- hundredDaysOfCode/intermediate/day_16/oop_coffee_machine/main.py
+++ hundredDaysOfCode/intermediate/day_16/oop_coffee_machine/main.py
@@ -3,10 +3,15 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
 menu = Menu()
 machine_is_on = True
+
+
 while machine_is_on:
     drink = input(f"What would you linke {menu.get_items()}: ")
     if drink == "off":
         machine_is_on = False
-    coffee_maker.report()
+    elif drink == "report":
+        money_machine.report()
+        coffee_maker.report()
