cb9f156ebce975a9462f5218f2a8df601181283d
Fikasentani <mpilo.dlamini@yahoo.com>
2022-06-26 03:55:16 +0200

    day-16: checking if payment is enough for coffee before making coffee

--- hundredDaysOfCode/intermediate/day_16/oop_coffee_machine/main.py
+++ hundredDaysOfCode/intermediate/day_16/oop_coffee_machine/main.py
@@ -3,9 +3,9 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 DRINK_PRICE = {
-"espresso": 1.5,
-"latte": 2.5,
-"cappuccino": 3.0
+    "espresso": 1.5,
+    "latte": 2.5,
+    "cappuccino": 3.0
 }
 coffee_maker = CoffeeMaker()
 money_machine = MoneyMachine()
@@ -13,14 +13,13 @@ menu = Menu()
 
 machine_is_on = True
 
-
 while machine_is_on:
     drink = input(f"What would you like {menu.get_items()}: ")
     print(menu.find_drink(drink).ingredients['water'])
     water = menu.find_drink(drink).ingredients['water']
     milk = menu.find_drink(drink).ingredients['milk']
     coffee = menu.find_drink(drink).ingredients['coffee']
-
+    cost = DRINK_PRICE[drink]
 
     if drink == "off":
         machine_is_on = False
@@ -28,9 +27,10 @@ while machine_is_on:
         money_machine.report()
         coffee_maker.report()
     else:
-        menu_item = MenuItem(drink, water, milk, coffee, DRINK_PRICE[drink])
+        menu_item = MenuItem(drink, water, milk, coffee, cost)
         print(coffee_maker.is_resource_sufficient(menu_item))
-        if coffee_maker.is_resource_sufficient(menu_item):
-            coffee_maker.make_coffee(menu_item)
-        else:
+        if not coffee_maker.is_resource_sufficient(menu_item):
             print(f"Sorry, there is not enough {menu_item}")
+        else:
+            if money_machine.make_payment(cost):
+                coffee_maker.make_coffee(menu_item)
