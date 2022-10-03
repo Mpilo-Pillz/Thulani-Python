acf5c53a7f54ce37ffe61ea27740653c6084023b
Fikasentani <mpilo.dlamini@yahoo.com>
2022-10-03 12:12:45 +0200

    day 18 learning turtle graphics

---
+++ hundredDaysOfCode/intermediate/day_18/turtle_grapics_theory.py
@@ -0,0 +1,7 @@
+from turtle import Turtle, Screen
+
+thulani_the_turtile = Turtle()
+thulani_the_turtile.shape("turtle")
+
+screen = Screen()
+screen.exitonclick()
---
+++ hundredDaysOfCode/intermediate/day_18/turtle_square.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen
+
+tshepho_the_turtle = Turtle()
+
+tshepho_the_turtle.forward(100)
+tshepho_the_turtle.left(90)
+tshepho_the_turtle.forward(100)
+tshepho_the_turtle.left(90)
+tshepho_the_turtle.forward(100)
+tshepho_the_turtle.left(90)
+tshepho_the_turtle.forward(100)
+
+screen = Screen()
+screen.exitonclick()
