d38bf97a591c56eb0edf54eedd59fe631e0c4186
Fikasentani <mpilo.dlamini@yahoo.com>
2022-10-06 12:39:24 +0200

    adding solution to turtle square

--- hundredDaysOfCode/intermediate/day_18/turtle_square_solution.py
+++ hundredDaysOfCode/intermediate/day_18/turtle_square_solution.py
@@ -1,11 +1,12 @@
 from turtle import Turtle, Screen
-
+import heroes
 
 lifudfvu = Turtle()
 
 for _ in range(4):
     lifudfvu.forward(100)
     lifudfvu.left(90)
+
 
 screen = Screen()
 screen.exitonclick()
