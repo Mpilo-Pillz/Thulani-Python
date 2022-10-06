5e2657a3ed0b14a61724dc3b9a5a3dd0d818a5af
Fikasentani <mpilo.dlamini@yahoo.com>
2022-10-06 12:38:36 +0200

    learning how to import external libraries

--- hundredDaysOfCode/intermediate/day_18/turtle_grapics_theory.py
+++ hundredDaysOfCode/intermediate/day_18/turtle_grapics_theory.py
@@ -1,7 +1,10 @@
 from turtle import Turtle, Screen
 
+import heroes
+
 thulani_the_turtile = Turtle()
 thulani_the_turtile.shape("turtle")
 
+print(heroes.gen())
 screen = Screen()
 screen.exitonclick()
