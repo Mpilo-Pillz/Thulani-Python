75a34ba2cc49901b6f072d06e8d2bd9c88184c09
Fikasentani <mpilo.dlamini@yahoo.com>
2022-10-30 14:40:54 +0200

    day-18:  drawing other shapes from 3 sides to 10 sides

--- hundredDaysOfCode/intermediate/day_18/turtleshapes_solution.py
+++ hundredDaysOfCode/intermediate/day_18/turtleshapes_solution.py
@@ -4,7 +4,12 @@ tshepiso = t.Turtle()
 
 num_sides = 5 #draw pentagon
 
-for _ in range(num_sides):
+def draw_shape(num_sides):
     angle = 360 / num_sides
-    tshepiso.forward(100)
-    tshepiso.right(angle)
+    for _ in range(num_sides):
+        tshepiso.forward(100)
+        tshepiso.right(angle)
+
+# loop between numbers 3 and 10. 11 is excluded
+for shape_side_n in  range(3, 11):
+    draw_shape(shape_side_n)
