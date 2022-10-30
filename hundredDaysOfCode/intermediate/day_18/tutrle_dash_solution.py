0bc213fbb4a7eefd77d0d009ebfddceeb8e68b5f
Fikasentani <mpilo.dlamini@yahoo.com>
2022-10-30 14:31:42 +0200

    turtle dash solution

---
+++ hundredDaysOfCode/intermediate/day_18/tutrle_dash_solution.py
@@ -0,0 +1,10 @@
+import turtle as t
+
+tetfutonkhe = t.Turtle()
+
+for _ in range(15):
+    tetfutonkhe.forward(10)
+    tetfutonkhe.penup()
+    tetfutonkhe.forward(10)
+    tetfutonkhe.pendown()
+
